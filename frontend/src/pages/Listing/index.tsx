eb5b942b992e29062c9ac1d2bb4bdf6fce5ca25d
Arthur Rezende <arthurrezende0212@gmail.com>
2022-03-11 14:29:00 -0300

    First request

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,9 +1,14 @@
+import axios from "axios";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
 
 function Listing() {
 
-
+    ///forma errada
+    axios.get('http://localhost:8080/movies/1')
+        .then(response => {
+            console.log(response.data);
+        })
 
     return (
         <>
